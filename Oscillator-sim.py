11bffd5538730969cb673e0d8b27967b4c54ebbf
mr-sagarmandal <sagar187@hotmail.com>
2018-08-01 23:21:34 -0400

    Added License file and Main file

---
+++ Oscillator-sim.py
@@ -0,0 +1,13 @@
+"""
+Copyright (C) 2018 Sagar Mandal - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the GNU GPLv3 license.
+
+You should have received a copy of the GNU GPLv3 license with
+this file. If not, please write to: sagar187@hotmail.com.
+"""
+
+import matplotlib as plt
+
+
+
